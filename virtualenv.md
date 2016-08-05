4fe82b90efc188fbf61633be578848df14f95053
gengzhibo <gengzhibo@gengzhibo.net>
2016-08-05 18:17:59 +0800

    python验证码识别

---
+++ virtualenv.md
@@ -0,0 +1,3 @@
+virtualenv --no-site-packages venv 创建virtualenv环境
+
+deactivate 退出环境
---
+++ yanzhengma2/yanzhengma.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytesseract
+from PIL import Image
+
+
+def initTable(threshold=140):
+    table = []
+    for i in range(256):
+        if i < threshold:
+            table.append(0)
+        else:
+            table.append(1)
+    return table
+
+
+
+im = Image.open('Captcha.jpg')
+
+
+im = im.convert('L')
+binaryImage = im.point(initTable(),'1')
+#binaryImage.show()
+
+print(pytesseract.image_to_string(binaryImage , config='-psm 7'))
