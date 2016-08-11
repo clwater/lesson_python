87ac0bbf5d85c0563b4ac1af75db2313925c1141
gengzhibo <gengzhibo@gengzhibo.net>
2016-08-11 13:41:34 +0800

    Blog

--- blog/hello.py
+++ blog/hello.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask import Flask, render_template, session, redirect, url_for , flash
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to gues string'
 
-@app.route('/')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/' , methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html' , form = form , name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
@@ -23,6 +36,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 if __name__ == '__main__':
     app.run()
